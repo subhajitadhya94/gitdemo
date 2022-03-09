3b9d00c06dcbcb4477158098ad6ee0a8aea639ad
subhajitadhya94 <subhajitadhya94@gmail.com>
2022-03-09 20:28:06 +0530

    commit done by USA guy

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -7,16 +7,19 @@ public class AppiumTest {
 	@Test
 	public void NativeAppAndroid() {
 		System.out.println("NativeAppAndroid");
+		System.out.println("NativeAppAndroid by USA");
 	}
 	
 	@Test
 	public void IosApps() {
 		System.out.println("IosApps");
+		System.out.println("IosApps by USA");
 	}
 	
 	@Test
 	public void subhajit() {
 		System.out.println("Subhajit Adhya");
+		System.out.println("Subhajit Adhya by USA");
 	}
 
 }
